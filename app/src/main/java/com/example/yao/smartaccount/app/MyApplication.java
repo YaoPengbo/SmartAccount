b32900da493bc29b1cb2ef00d7cbce9719a7cdc3
YaoPengbo <578596277@qq.com>
2016-11-07 00:14:56 +0800

    first commit

---
+++ app/src/main/java/com/example/yao/smartaccount/app/MyApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Created by Pengbo Yao on 16-11-6 下午11:50
+ * Copyright (c) 2016.  All rights reserved
+ *
+ * Last modified 16-11-6 下午11:50
+ */
+
+package com.example.yao.smartaccount.app;
+
+import android.app.Application;
+
+/**
+ * Author  : Pengbo Yao(yaopengbo1@gmail.com)
+ * Date    : 2016-11-06
+ * Time    : 23:50
+ * function:
+ */
+
+public class MyApplication extends Application {
+}
